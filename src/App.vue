de1add2526555d7fa30c44f95680b02a3bca38ba
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-12-05 14:44:35 +0600

    if else directives

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ export default {
   data() {
     return {
       name: 'John Doe',
+      status: 'pending',
     }
   }
 }
@@ -10,5 +11,8 @@ export default {
 
 <template>
   <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
 </template>
 
