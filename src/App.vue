55ebea83521999426238a476130ffc5db05f653c
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-12-05 14:48:29 +0600

    v for directive looping

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ export default {
     return {
       name: 'John Doe',
       status: 'pending',
+      tasks: ['Task One', 'TaskTwo', 'Task Three']
     }
   }
 }
@@ -14,5 +15,10 @@ export default {
   <p v-if="status === 'active'">User is active</p>
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
 </template>
 
